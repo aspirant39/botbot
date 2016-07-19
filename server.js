0fbaab7f4904ef35c01c2aac0de1cf0e4c88928b
aspirant39 <aspirant_programmer@yahoo.com>
2016-07-19 10:59:50 +0800

    Update server.js

--- server.js
+++ server.js
@@ -23,23 +23,12 @@ server.post('/api/messages', connector.listen());
 // Bots Dialogs
 //=========================================================
 
-bot.dialog('/^start/i', [
+var intents = new builder.IntentDialog();
+bot.dialog('/', intents);
+
+intents.matches(/^Start/i, [
     function (session) {
-        // Send a greeting and start the menu.
-        var card = new builder.HeroCard(session)
-            .title("Microsoft Bot Framework")
-            .text("Your bots - wherever your users are talking.")
-            .images([
-                 builder.CardImage.create(session, "http://docs.botframework.com/images/demo_bot_image.png")
-            ]);
-        var msg = new builder.Message(session).attachments([card]);
-        session.send(msg);
-        session.send("Hi... I'm the Microsoft Bot Framework demo bot for Skype. I can show you everything you can use our Bot Builder SDK to do on Skype.");
         session.beginDialog('/menu');
-    },
-    function (session, results) {
-        // Always say goodbye
-        session.send("Ok... See you later!");
     }
 ]);
 
