023cfe488583aeb4dab3f3580680841fbf674fa1
aspirant39 <aspirant_programmer@yahoo.com>
2016-07-19 10:52:09 +0800

    Update server.js

--- server.js
+++ server.js
@@ -43,4 +43,23 @@ bot.dialog('/', [
     }
 ]);
 
+bot.dialog('/menu', [
+    function (session) {
+        builder.Prompts.choice(session, "What demo would you like to run?", "prompts|picture|cards|list|carousel|receipt|(quit)");
+    },
+    function (session, results) {
+        if (results.response && results.response.entity != '(quit)') {
+            // Launch demo dialog
+            session.beginDialog('/' + results.response.entity);
+        } else {
+            // Exit the menu
+            session.endDialog();
+        }
+    },
+    function (session, results) {
+        // The menu runs a loop until the user chooses to (quit).
+        session.replaceDialog('/menu');
+    }
+]);
+
 
