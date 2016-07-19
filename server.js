e0927485110376cceed8c22d1c08562d40431fb2
aspirant39 <aspirant_programmer@yahoo.com>
2016-07-19 11:14:43 +0800

    Update server.js

--- server.js
+++ server.js
@@ -119,16 +119,5 @@ bot.dialog('/Ask', [
     }
 ]);
 
-bot.dialog('/Answer', [
-    function (session) {
-        session.send("You can easily send pictures to a user...");
-        var msg = new builder.Message(session)
-            .attachments([{
-                contentType: "image/jpeg",
-                contentUrl: "http://www.theoldrobots.com/images62/Bender-18.JPG"
-            }]);
-        session.endDialog(msg);
-    }
-]);
 
 
