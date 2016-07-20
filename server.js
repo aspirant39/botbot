99f967033b343559135348f0d394833cb374a328
aspirant39 <aspirant_programmer@yahoo.com>
2016-07-20 12:04:12 +0800

    Update server.js

--- server.js
+++ server.js
@@ -19,33 +19,102 @@ var connector = new builder.ChatConnector({
 var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
 
-//=========================================================
-// Bots Dialogs
-//=========================================================
 
+var intents = new builder.IntentDialog();
+bot.dialog('/', intents);
 
-bot.dialog('/',function(session){
-   session.send("Hi %s, ",session.userData.name);
-});
-bot.use({
-   dialog: function(session,next){
-      if(!session.userData.firstRun){
-         seesion.userData.firstRun=true;
-         session.beginDialog('/firstRun');
-      }
-      else{
-         next();
-      }
-   }
-});
-bot.dialog('/firstRun', [
-    function (session) {
-        builder.Prompts.choice(session, "What would you like me to do?", "prompts|list|(quit)");
+intents.onDefault([
+    function (session, args, next) {
+        if (!session.userData.name) {
+            session.beginDialog('/profile');
+        } else {
+            next();
+        }
     },
     function (session, results) {
+        session.send('Hello %s!', session.userData.name);
+        session.beginDialog('/menu');
+        
+    }
+]);
+bot.on('conversationUpdate', function (message) {
+   // Check for group conversations
+    if (message.address.conversation.isGroup) {
+        // Send a hello message when bot is added
+        if (message.membersAdded) {
+           message.membersAdded.forEach(function (identity) {
+               if (identity.id === message.address.bot.id) {
+                    var reply = new builder.Message()
+                            .address(message.address)
+                           .text("Hello everyone!");
+                   bot.send(reply);
+                }
+            });
+       }
+
+        // Send a goodbye message when bot is removed
+        if (message.membersRemoved) {
+            message.membersRemoved.forEach(function (identity) {
+                if (identity.id === message.address.bot.id) {
+                   var reply = new builder.Message()
+                        .address(message.address)
+                        .text("Goodbye");
+                    bot.send(reply);
+                }
+            });
+        }
+    }
+});
+
+bot.on('contactRelationUpdate', function (message) {
+    if (message.action === 'add') {
+        var name = message.user ? message.user.name : null;
+        var reply = new builder.Message()
+                .address(message.address)
+               .text("Hello %s... Thanks for adding me. ", name || 'there');
+        bot.send(reply);
+    } else {
+        // delete their data
+    }
+});
+
+bot.on('typing', function (message) {
+    // User is typing
+});
+
+bot.on('deleteUserData', function (message) {
+   // User asked to delete their data
+});
+
+bot.use(downloadFile(connector));
+
+bot.use(builder.Middleware.dialogVersion({ version: 1.0, resetCommand: /^reset/i }));
+
+bot.dialog('/profile', [
+    function (session) {
+        builder.Prompts.text(session, 'Hi! What is your name?');
+    },
+    function (session, results) {
+        session.userData.name = results.response;
+        session.endDialog();
+    }
+]);
+
+bot.dialog('/menu', [
+    function (session) {
+       builder.Prompts.choice(session, "What would you like me to do?", "Ask|Answer|(quit)");
+   },
+   function (session, results) {
         if (results.response && results.response.entity != '(quit)') {
-            // Launch demo dialog
-            session.beginDialog('/' + results.response.entity);
+            switch (results.response.entity) {
+                case 'Ask':
+                    session.beginDialog('/Ask');
+                    break;
+               case 'Asnwer':
+                    session.beginDialog('Answer');
+                    break; 
+
+            }
         } else {
             // Exit the menu
             session.endDialog();
@@ -53,9 +122,6 @@ bot.dialog('/firstRun', [
     },
     function (session, results) {
         // The menu runs a loop until the user chooses to (quit).
-        session.replaceDialog('/firstRun');
-    }
-]);
-
-
-
+        session.replaceDialog('/menu');
+       }
+])
