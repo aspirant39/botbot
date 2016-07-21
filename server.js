4ce82a55bfe5da4efa5f3bdbb7527395d7493919
aspirant39 <aspirant_programmer@yahoo.com>
2016-07-21 13:16:48 +0800

    Update server.js

--- server.js
+++ server.js
@@ -18,57 +18,9 @@ var connector = new builder.ChatConnector({
 });
 var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
-bot.on('conversationUpdate', function (message) {
-   // Check for group conversations
-    if (message.address.conversation.isGroup) {
-        // Send a hello message when bot is added
-        if (message.membersAdded) {
-            message.membersAdded.forEach(function (identity) {
-                if (identity.id === message.address.bot.id) {
-                    var reply = new builder.Message()
-                            .address(message.address)
-                            .text("Hello everyone!");
-                    bot.send(reply);
-                }
-            });
-        }
-
-        // Send a goodbye message when bot is removed
-        if (message.membersRemoved) {
-            message.membersRemoved.forEach(function (identity) {
-                if (identity.id === message.address.bot.id) {
-                    var reply = new builder.Message()
-                        .address(message.address)
-                        .text("Goodbye");
-                    bot.send(reply);
-                }
-            });
-        }
-    }
-});
-bot.on('contactRelationUpdate', function (message) {
-    if (message.action === 'add') {
-        var name = message.user ? message.user.name : null;
-        var reply = new builder.Message()
-                .address(message.address)
-                .text("Hello %s... Thanks for adding me.", name || 'there');
-        bot.send(reply);
-    } else {
-        // delete their data
-    }
-});
-bot.on('typing', function (message) {
-    // User is typing
-});
-
-bot.on('deleteUserData', function (message) {
-    // User asked to delete their data
-});
 
 var intents = new builder.IntentDialog();
-
-
-bot.use(builder.Middleware.dialogVersion({ version: 3.0, resetCommand: /^reset/i }));
+bot.use(builder.Middleware.dialogVersion({ version: 1.0, resetCommand: /^reset/i }));
 bot.use(downloadFile(connector));
 bot.dialog('/', intents);
 
