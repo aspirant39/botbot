dbb6d5909616c5d752df6b5c164e761d5e856f19
aspirant39 <aspirant_programmer@yahoo.com>
2016-08-02 10:44:24 +0800

    Update server.js

--- server.js
+++ server.js
@@ -94,6 +94,67 @@ bot.dialog('/menu', [
         session.replaceDialog('/menu');
        }
 ]);
+
+
+bot.dialog('/Answer', [
+    function (session) {
+        session.send("Ask me anything. Just follow the prompts and you can quit at any time by saying 'cancel'.");
+        builder.Prompts.text(session, "\n\nwhat would you like to ask me?");
+    },
+    function (session, results) {
+        if (results && results.response) {
+            if(results.response=='Author'){
+                 session.send("Si kuan ay! Si Ralph gud (facepalm)");
+              
+            }
+            else if(results.response=='Version'){
+                 session.send("Version 007(facepalm)");
+                
+            }
+            else if(results.response!='cancel'){
+               session.send("Ambot lang ui(facepalm)");
+            }
+            else{
+               session.endDialog("You canceled.");
+            }
+            
+        } else if(results.response=='cancel') {
+            session.endDialog("You canceled.");
+        }
+         else {
+            
+        }
+    },
+    function (session, results) {
+        // The menu runs a loop until the user chooses to (quit).
+        session.replaceDialog('/Answer');
+       }
+]);
+
+
+bot.dialog('/actions', [
+    function (session) { 
+        session.send("BOTBOT");
+
+        var msg = new builder.Message(session)
+            .textFormat(builder.TextFormat.xml)
+            .attachments([
+                new builder.HeroCard(session)
+                    .title("Menu)
+                    .subtitle("choose action")
+                    .text("What would you like me to do?")
+                    .images([
+                        builder.CardImage.create(session, "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7c/Seattlenighttimequeenanne.jpg/320px-Seattlenighttimequeenanne.jpg")
+                    ])
+                    .buttons([
+                        builder.CardAction.dialogAction(session, "Ask", "Asks", "Askss")
+                    ])
+            ]);
+        session.send(msg);
+
+        session.endDialog("The 'Current Weather' button on the card above can be pressed at any time regardless of where the user is in the conversation with the bot. The bot can even show the weather after the conversation has ended.");
+    }
+]);
 bot.dialog('/Ask', [
     function (session,args) {
         session.send("i will ask you some questions . Just follow the prompts and you can quit at any time by saying 'cancel'. %s",args.data);
@@ -146,100 +207,6 @@ bot.dialog('/Ask', [
         session.replaceDialog('/Ask');
        }
 ]);
-
-bot.dialog('/Answer', [
-    function (session) {
-        session.send("Ask me anything. Just follow the prompts and you can quit at any time by saying 'cancel'.");
-        builder.Prompts.text(session, "\n\nwhat would you like to ask me?");
-    },
-    function (session, results) {
-        if (results && results.response) {
-            if(results.response=='Author'){
-                 session.send("Si kuan ay! Si Ralph gud (facepalm)");
-              
-            }
-            else if(results.response=='Version'){
-                 session.send("Version 007(facepalm)");
-                
-            }
-            else if(results.response!='cancel'){
-               session.send("Ambot lang ui(facepalm)");
-            }
-            else{
-               session.endDialog("You canceled.");
-            }
-            
-        } else if(results.response=='cancel') {
-            session.endDialog("You canceled.");
-        }
-         else {
-            
-        }
-    },
-    function (session, results) {
-        // The menu runs a loop until the user chooses to (quit).
-        session.replaceDialog('/Answer');
-       }
-]);
-
-bot.dialog('/cards', [
-    function (session) {
-        session.send("You can use Hero & Thumbnail cards to send the user a visually rich information...");
-
-        var msg = new builder.Message(session)
-            .textFormat(builder.TextFormat.xml)
-            .attachments([
-                new builder.HeroCard(session)
-                    .title("Hero Card")
-                    .subtitle("Space Needle")
-                    .text("The <b>Space Needle</b> is an observation tower in Seattle, Washington, a landmark of the Pacific Northwest, and an icon of Seattle.")
-                    .images([
-                        builder.CardImage.create(session, "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7c/Seattlenighttimequeenanne.jpg/320px-Seattlenighttimequeenanne.jpg")
-                    ])
-                    .tap(builder.CardAction.openUrl(session, "https://en.wikipedia.org/wiki/Space_Needle"))
-            ]);
-        session.send(msg);
-
-        msg = new builder.Message(session)
-            .textFormat(builder.TextFormat.xml)
-            .attachments([
-                new builder.ThumbnailCard(session)
-                    .title("Thumbnail Card")
-                    .subtitle("Pikes Place Market")
-                    .text("<b>Pike Place Market</b> is a public market overlooking the Elliott Bay waterfront in Seattle, Washington, United States.")
-                    .images([
-                        builder.CardImage.create(session, "https://upload.wikimedia.org/wikipedia/en/thumb/2/2a/PikePlaceMarket.jpg/320px-PikePlaceMarket.jpg")
-                    ])
-                    .tap(builder.CardAction.openUrl(session, "https://en.wikipedia.org/wiki/Pike_Place_Market"))
-            ]);
-        session.endDialog(msg);
-    }
-]);
-
-bot.dialog('/actions', [
-    function (session) { 
-        session.send("BOTBOT");
-
-        var msg = new builder.Message(session)
-            .textFormat(builder.TextFormat.xml)
-            .attachments([
-                new builder.HeroCard(session)
-                    .title("Menu)
-                    .subtitle("choose action")
-                    .text("What would you like me to do?")
-                    .images([
-                        builder.CardImage.create(session, "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7c/Seattlenighttimequeenanne.jpg/320px-Seattlenighttimequeenanne.jpg")
-                    ])
-                    .buttons([
-                        builder.CardAction.dialogAction(session, "Ask", "Asks", "Askss")
-                    ])
-            ]);
-        session.send(msg);
-
-        session.endDialog("The 'Current Weather' button on the card above can be pressed at any time regardless of where the user is in the conversation with the bot. The bot can even show the weather after the conversation has ended.");
-    }
-]);
-
 bot.beginDialogAction('Ask', '/Ask');
 
 var fs = require('fs');
