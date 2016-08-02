18b95fa7f6b8a7b1d8d5f3696275b6fd76b529e8
aspirant39 <aspirant_programmer@yahoo.com>
2016-08-02 10:58:13 +0800

    Update server.js

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ intents.onDefault([
     },
     function (session, results) {
         session.send('Hello %s!', session.userData.name);
-        session.beginDialog('/actions');
+       
         
     }
 ]);
@@ -48,36 +48,6 @@ bot.dialog('/profile', [
         session.endDialog();
     }
 ]);
-
-bot.dialog('/actions', [
-    function (session) { 
-        session.send("BOTBOT");
-
-        var msg = new builder.Message(session)
-            .textFormat(builder.TextFormat.xml)
-            .attachments([
-                new builder.HeroCard(session)
-                    .title("Menu)
-                    .subtitle("choose action")
-                    .text("What would you like me to do?")
-                    .images([
-                        builder.CardImage.create(session, "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7c/Seattlenighttimequeenanne.jpg/320px-Seattlenighttimequeenanne.jpg")
-                    ])
-                    .buttons([
-                        builder.CardAction.dialogAction(session, "Ask", "Asks", "Askss")
-                    ])
-            ]);
-        session.send(msg);
-
-        session.endDialog("The 'Current Weather' button on the card above can be pressed at any time regardless of where the user is in the conversation with the bot. The bot can even show the weather after the conversation has ended.");
-    }
-]);
-bot.dialog('/Ask', [
-    function (session,args) {
-        session.endDialog("i will ask you some questions . Just follow the prompts and you can quit at any time by saying 'cancel'. %s",args.data);
-    }
-]);
-bot.beginDialogAction('Ask', '/Ask');
 
 var fs = require('fs');
 var request = require('request');
