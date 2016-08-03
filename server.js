6b62e95bbf6b4297d3b6b2359229067143619cb1
aspirant39 <aspirant_programmer@yahoo.com>
2016-08-03 14:51:39 +0800

    Update server.js

--- server.js
+++ server.js
@@ -114,7 +114,7 @@ bot.dialog('/actions', [
                         builder.CardImage.create(session, "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7c/Seattlenighttimequeenanne.jpg/320px-Seattlenighttimequeenanne.jpg")
                     ])
                     .buttons([
-                        builder.CardAction.dialogAction(session,session.dailogData, "Ask", "Ask")
+                        builder.CardAction.dialogAction("ask", "Ask", "Ask")
                     ])
             ]);
         session.send(msg);
